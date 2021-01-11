0ff86fb2c6356a11d09b64f776f95044df43c603
mlobf <marcoslemeborbafilho@gmail.com>
2021-01-11 18:28:12 -0300

    End of Events and Methods

---
+++ Events_Methods/main.js
@@ -0,0 +1,20 @@
+var app = new Vue({
+  el: "#app",
+  data: {
+    lesson: "Events and Methods",
+    counter: 0,
+  },
+  methods: {
+    incrementCounter() {
+      this.counter += 1;
+      console.log(this.counter, "Vamos la!!!");
+
+      if (this.counter === 10) {
+        alert("Counter is at 10!");
+      }
+    },
+    overTheBox() {
+      console.log("Over The Green Box!");
+    },
+  },
+});
