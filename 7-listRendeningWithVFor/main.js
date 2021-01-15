e0ed49f00b1459aeb744e1c7fa4c9fe88059a456
mlobf <marcoslemeborbafilho@gmail.com>
2021-01-14 23:29:28 -0300

    last commit

--- 7-listRendeningWithVFor/main.js
+++ 7-listRendeningWithVFor/main.js
@@ -1,7 +1,19 @@
-// Lets Rock
 var app = new Vue({
   el: "#app",
-  data() {
-    users: ["alice", "bob", "batman", "robin", "superman"];
+  data: {
+    users: [
+      {
+        id: 567,
+        name: "alice",
+        profession: "developer",
+      },
+      {
+        id: 567,
+        name: "alice",
+        profession: "developer",
+      },
+    ],
   },
 });
+
+// users: ["alice", "bob", "batman", "robin", "superman"],
